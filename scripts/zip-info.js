d21c4c3ff2c3b655b85d9b1cddd8b4133de749c8
Sergey Antonov <clark.kent.2211@yandex.ru>
2024-09-06 23:47:26 +0300

    Debug/release mode

--- scripts/zip-info.js
+++ scripts/zip-info.js
@@ -4,8 +4,10 @@ const targetSize = 13312;
 
 const stats = fs.statSync("dist/cube13.zip");
 
+const percent = (stats.size / targetSize * 100.0).toFixed(2);
+
 if (stats.size <= targetSize) {
-    console.log(`GOOD ZIP SIZE: ${stats.size} of ${targetSize}`);
+    console.log(`GOOD ZIP SIZE: ${stats.size} / ${targetSize} (${percent}%)`);
 } else {
-    console.log(`WARNING! BAD ZIP SIZE: ${stats.size} of ${targetSize}`);
+    console.log(`ALERT!!! BAD ZIP SIZE: ${stats.size} / ${targetSize} (${percent}%)`);
 }
