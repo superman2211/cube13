c356a21c8bee0192e061486710159120afa1c11a
Sergey Antonov <clark.kent.2211@yandex.ru>
2024-11-17 17:41:05 +0300

    Implement PWA

--- dist/build/sw.js
+++ dist/build/sw.js
@@ -1,4 +1,36 @@
-console.log('hello from sw.js'); // this should not be printed because sw is not launched by itself, it calls the browser (to be confirmed)
-self.addEventListener('install', function(event) {console.log('SW installed', event);});
-self.addEventListener('fetch', function(event) {console.log('SW fetched', event);});
-self.addEventListener('activate', function(event) {console.log('SW activated', event);});
+
+const cacheName = "static-v1";
+const contentToCache = [
+    "/r",
+    "/s.js",
+];
+
+self.addEventListener("install", (e) => {
+    console.log("[Service Worker] Install");
+    e.waitUntil(
+        (async () => {
+            const cache = await caches.open(cacheName);
+            console.log("[Service Worker] Caching all: app shell and content");
+            await cache.addAll(contentToCache);
+        })(),
+    );
+});
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        (async () => {
+            const r = await caches.match(e.request);
+            console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+            if (r) {
+                return r;
+            }
+            const response = await fetch(e.request);
+            const cache = await caches.open(cacheName);
+            console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+            cache.put(e.request, response.clone());
+            return response;
+        })(),
+    );
+});
+
+self.addEventListener('activate', function (event) { console.log('[Service Worker] activated', event); });
